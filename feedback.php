c6049d47e926b976f64befd95a46f4387865a63c
Kelvin Gitari <90972218+Kelvin-Gitari@users.noreply.github.com>
2023-03-13 17:23:00 +0300

    A Feedback App-Connected to database

---
+++ feedback.php
@@ -0,0 +1,29 @@
+<?php include 'include/header.php'; ?>
+
+<?php
+    $sql = 'SELECT * FROM feedback';
+    $result = mysqli_query($conn, $sql);
+    $feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+   
+    <h2>Feedback</h2>
+
+    <?php if(empty($feedback)): ?>
+
+        <p class="lead mt3">There is no feedback</p>
+    
+        <?php endif; ?>
+
+    <?php foreach($feedback as $item): ?>
+        <div class="card mt-3 w-75">
+            <div class="card-body text-center">
+                <?php echo $item['body']; ?>
+                <div class="text-secondary mt-2">
+                    By <?php echo $item['name']; ?> on <?php echo $item['date']; ?>
+</div>
+</div>
+</div>
+
+<?php endforeach; ?>
+
+   <?php include 'include/footer.php'; ?> 
